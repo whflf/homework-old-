1cc7af3b14ed0d0d09375586d9b2f0fbaf2e4610
FluffyJesus <79448063+FluffyJesus@users.noreply.github.com>
2023-09-14 22:45:39 +0300

    Update task1.c

--- task1.c
+++ task1.c
@@ -1,65 +1,26 @@
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
 
-
-int arraySwap(int array[], int m, int n)
-{
-    for (int i = 0; i < m / 2 + 1; i++)
-    {
-        if (m % 2 == 1 && i == m / 2)
-        {
-            break;
-        }
-        int auxiliaryVariable = array[m - i - 1];
-        array[m - i - 1] = array[i];
-        array[i] = auxiliaryVariable;
-    }
-    int j = 0;
-    for (int i = m; i < m + n / 2 + 1; i++)
-    {
-        if (n % 2 == 1 && i == m + n / 2)
-        {
-            break;
-        }
-        int auxiliaryVariable = array[m + n - j - 1];
-        array[m + n - j - 1] = array[i];
-        array[i] = auxiliaryVariable;
-        j += 1;
-    }
-    for (int i = 0; i < m + n; i++)
-    {
-        if ((m + n) % 2 == 1 && i == (m + n) / 2)
-        {
-            break;
-        }
-        int auxiliaryVariable = array[m + n - i - 1];
-        array[m + n - i - 1] = array[i];
-        array[i] = auxiliaryVariable;
-    }
-    return array[m + n];
-}
-
-
-int main()
+int main(void)
 {
     int length = 0;
-    int m, n;
     printf("length = ");
     scanf_s("%d", &length);
     int* numbers;
-    numbers = (int*)malloc(length * sizeof(int) * 10);
+    numbers = (int*)malloc(length * sizeof(int));
     for (int i = 0; i < length; i++)
     {
         printf("numbers[%d] = ", i);
         scanf_s("%d", &numbers[i]);
     }
-    printf("m = ");
-    scanf_s("%d", &m);
-    printf("length = ");
-    scanf_s("%d", &n);
-    arraySwap(numbers, m, n);
-    for (int i = 0; i < n; ++i)
-        printf("%d ", numbers[i]);
-    free(numbers);
+    int amountOfZeroes = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (numbers[i] == 0)
+        {
+            amountOfZeroes += 1;
+        }
+    }
+    printf("%d ", amountOfZeroes);
     return 0;
 }
