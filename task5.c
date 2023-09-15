4fa7f9781f8ac76fad94270794775c59d1d8c63d
FluffyJesus <79448063+FluffyJesus@users.noreply.github.com>
2023-09-15 22:50:37 +0300

    Update task5.c

--- task5.c
+++ task5.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
-#include <conio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 #define LENGTH 100
 
 int main()
 {
-	char string[LENGTH];
-	printf("string = ");
-	fgets(string, LENGTH, stdin);
-	int count = 0;
-	for (int i = 0; i < strlen(string); i++)
-	{
-		if (string[i] == '(')
-		{
-			count += 1;
-		}
-		if (string[i] == ')')
-		{
-			count -= 1;
-			if (count < 0)
-			{
-				break;
-			}
-		}
-	}
-	if (count == 0) 
-	{
-		printf("ok");
-	}
-	else 
-	{
-		printf("not ok");
-	}
-	return 0;
+    char string[LENGTH];
+    printf("string = ");
+    fgets(string, LENGTH, stdin);
+    int count = 0;
+    for (int i = 0; i < strlen(string); i++)
+    {
+        if (string[i] == '(')
+        {
+            count += 1;
+        }
+        if (string[i] == ')')
+        {
+            count -= 1;
+            if (count < 0)
+            {
+                break;
+            }
+        }
+    }
+    if (count == 0) 
+    {
+        printf("ok");
+    }
+    else 
+    {
+        printf("not ok");
+    }
+    return 0;
 }
