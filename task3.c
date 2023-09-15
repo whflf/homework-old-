f1c7ba7ccc4f5f1a6523070b3448e69b375c5d9b
FluffyJesus <79448063+FluffyJesus@users.noreply.github.com>
2023-09-15 22:45:42 +0300

    Update task3.c

--- task3.c
+++ task3.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdbool.h>
+
 main()
 {
-	int var1, var2;
-	printf("var1 = ");
-	scanf_s("%d", &var1);
-	printf("var2 = ");
-	scanf_s("%d", &var2); 
-	int answer = 0;
-	bool flag = false;
-	if (var1 < 0 ^ var2 < 0)
-	{
-		flag = true;
-	}
-	if (var2 == 0)
-	{
-		printf("Zero division is prohibited ");
-		return -1;
-	}
-	if (var1 < 0)
-	{
-		var1 = -var1;
-	}
-	if (var2 < 0)
-	{
-		var2 = -var2;
-	}
-	while (var1 > var2)
-	{
-		var1 -= var2;
-		answer += 1;
-	}
-	if (flag == true)
-	{
-		answer = -answer;
-	}
-	printf("%d ", answer);
+    int number1 = 0;
+    int number2 = 0;
+    printf("number1 = ");
+    scanf_s("%d", &number1);
+    printf("number2 = ");
+    scanf_s("%d", &number2);
+    int answer = 0;
+    bool flag = false;
+    if (number1 < 0 ^ number2 < 0)
+    {
+        flag = true;
+    }
+    if (number2 == 0)
+    {
+        printf("Zero division is prohibited ");
+        return -1;
+    }
+    if (number1 < 0)
+    {
+        number1 = -number1;
+    }
+    if (number2 < 0)
+    {
+        number2 = -number2;
+    }
+    while (number1 > number2)
+    {
+        number1 -= number2;
+        answer += 1;
+    }
+    if (flag == true)
+    {
+        answer = -answer;
+    }
+    printf("%d / %d = %d", number1, number2, answer);
 }
