a1019b503fd8d0e10f751bda41644af467806f3d
FluffyJesus <79448063+FluffyJesus@users.noreply.github.com>
2023-09-15 23:16:50 +0300

    Update task7.c

--- task7.c
+++ task7.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <math.h>
 #define LENGTH 100
 
 int main()
 {
-	char string[LENGTH], string1[LENGTH];
-	printf("s = ");
-	fgets(string, LENGTH, stdin);
-	printf("s1 = ");
-	fgets(string1, LENGTH, stdin);
-	int index = 0;
-	int count = 0;
-	for (int i = 0; i < strlen(string); i++) 
-	{
-		if (string[i] == string1[index]) 
-		{
-			index += 1;
-			if (index == strlen(string1) - 1) 
-			{
-				index = 0;
-				count += 1;
-			}
-		}
-		else if (index != 0) 
-		{
-			index = 0;
-		}
-	}
-	printf("%d", count);
-	return 0;
+    char string[LENGTH], string1[LENGTH];
+    printf("s = ");
+    fgets(string, LENGTH, stdin);
+    printf("s1 = ");
+    fgets(string1, LENGTH, stdin);
+    int index = 0;
+    int count = 0;
+    for (int i = 0; i < strlen(string); i++) 
+    {
+        if (string[i] == string1[index]) 
+        {
+            index += 1;
+            if (index == strlen(string1) - 1) 
+            {
+                index = 0;
+                count += 1;
+            }
+        }
+        else if (index != 0) 
+        {
+            index = 0;
+        }
+    }
+    printf("%d", count);
+    return 0;
 }
