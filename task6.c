6e857156f61b2cf19a2f1974914c324c4071719b
FluffyJesus <79448063+FluffyJesus@users.noreply.github.com>
2023-09-15 23:15:03 +0300

    Update task6.c

--- task6.c
+++ task6.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 #include <math.h>
-#define LENGTH 100
 
 int main()
 {
-	int number;
-	printf("number = ");
-	scanf_s("%d", &number);
-	for (int i = 0; i < number + 1; i++) 
-	{
-		if (i > 1) 
-		{
-			bool primeNumber = false;
-			for (int j = 2; j < round(sqrt(i)) + 1; j++) 
-			{
-				if (i % j == 0) 
-				{
-					primeNumber = true;
-					break;
-				}
-			}
-			if (primeNumber == false) 
-			{
-				printf("%d\n", i);
-			}
-		}
-	}
-	return 0;
+    int number;
+    printf("number = ");
+    scanf_s("%d", &number);
+    for (int i = 0; i < number + 1; ++i) 
+    {
+        if (i > 1) 
+        {
+            bool primeNumber = false;
+            for (int j = 2; j < round(sqrt(i)) + 1; ++j) 
+            {
+                if (i % j == 0) 
+                {
+                    primeNumber = true;
+                    break;
+                }
+            }
+            if (primeNumber == false) 
+            {
+                printf("%d\n", i);
+            }
+        }
+    }
 }
