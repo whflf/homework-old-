4e434876ef4da4f5306cbe921290a641383763cb
FluffyJesus <79448063+FluffyJesus@users.noreply.github.com>
2023-09-18 02:01:39 +0300

    Update task6.c

--- task6.c
+++ task6.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
 #include <math.h>
 
-int main()
+int primeNumbers(int number)
 {
-    int number;
-    printf("number = ");
-    scanf_s("%d", &number);
     for (int i = 0; i < number + 1; ++i) 
     {
         if (i > 1) 
         {
-            bool primeNumber = false;
-            for (int j = 2; j < round(sqrt(i)) + 1; ++j) 
+            bool primeNumber = true;
+            int divisors = 0;
+            for (int j = 2; j * j < i + 1; ++j) 
             {
                 if (i % j == 0) 
                 {
-                    primeNumber = true;
+                    ++divisors;
                     break;
                 }
             }
-            if (primeNumber == false) 
+        if (divisors) 
+        {
+            primeNumber = false;
+        }
+            if (primeNumber) 
             {
                 printf("%d\n", i);
             }
         }
     }
+}
+
+int main()
+{
+    int number = 0;
+    printf("number = ");
+    scanf_s("%d", &number);
+    primeNumbers(number);
 }
