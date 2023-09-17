2f58655aff320f4c82f3b805b4fe35adf3945c80
FluffyJesus <79448063+FluffyJesus@users.noreply.github.com>
2023-09-18 01:13:09 +0300

    Update task4.c

--- task4.c
+++ task4.c
@@ -5,21 +5,21 @@
 
 #define LENGTH 100
 
-int happyTicketsCount() 
+int happyTicketsCount()
 {
     int sums[28] = { 0 };
-    for (int i = 0; i < 10; i++) 
+    for (int i = 0; i < 10; i++)
     {
-        for (int j = 0; j < 10; ++j) 
+        for (int j = 0; j < 10; ++j)
         {
-            for (int k = 0; k < 10; ++k) 
+            for (int k = 0; k < 10; ++k)
             {
                 ++sums[i + j + k];
             }
         }
     }
     int happyTickets = 0;
-    for (int i = 0; i < 28; i++) 
+    for (int i = 0; i < 28; i++)
     {
         happyTickets += sums[i] * sums[i];
     }
