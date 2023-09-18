d3abf7f81a6b563be06d6cd27e3c43068682a740
FluffyJesus <79448063+FluffyJesus@users.noreply.github.com>
2023-09-18 21:43:25 +0300

    Update task7.c

--- task7.c
+++ task7.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
+
 #define LENGTH 100
 
-int main()
+int substringsCount(char string[], char string1[])
 {
-    char string[LENGTH], string1[LENGTH];
-    printf("s = ");
-    fgets(string, LENGTH, stdin);
-    printf("s1 = ");
-    fgets(string1, LENGTH, stdin);
     int index = 0;
     int count = 0;
-    for (int i = 0; i < strlen(string); i++) 
+    const stringLength = strlen(string);
+    for (int i = 0; i < stringLength; i++)
     {
-        if (string[i] == string1[index]) 
+        if (string[i] == string1[index])
         {
             index += 1;
-            if (index == strlen(string1) - 1) 
+            if (index == strlen(string1) - 1)
             {
                 index = 0;
                 count += 1;
             }
         }
-        else if (index != 0) 
+        else if (index != 0)
         {
             index = 0;
         }
     }
-    printf("%d", count);
+    return count;
+}
+
+int main()
+{
+    char string[LENGTH] = { '\0' };
+    char string1[LENGTH] = { '\0' };
+    printf("s = ");
+    fgets(string, LENGTH, stdin);
+    printf("s1 = ");
+    fgets(string1, LENGTH, stdin);
+    printf("S includes S1 %d times", substringsCount(string, string1));
     return 0;
 }
