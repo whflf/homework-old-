906ed5d030ab5d48947bcbea4e0e1373759864dd
FluffyJesus <79448063+FluffyJesus@users.noreply.github.com>
2023-09-19 00:16:12 +0300

    Update task8.c

--- task8.c
+++ task8.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
-#include <math.h>
-#define LENGTH 100
-
 
 int arraySwap(int array[], int m, int n)
 {
     int j = 0;
-    for (int i = m; i < m + n / 2 + 1; i++)
+    for (int i = m; i < m + n / 2 + 1; ++i)
     {
         if (n % 2 == 1 && i == m + n / 2)
         {
             break;
         }
-        int auxiliaryVariable = array[m + n - j - 1];
-        array[m + n - j - 1] = array[i];
-        array[i] = auxiliaryVariable;
-        j += 1;
+        array[i] = array[i] + array[m + n - j - 1];
+        array[m + n - j - 1] = array[i] - array[m + n - j - 1];
+        array[i] = array[i] - array[m + n - j - 1];
+        ++j;
     }
     return array;
 }
@@ -30,7 +26,7 @@ int main()
     int m, n;
     printf("length = ");
     scanf_s("%d", &length);
-    int *numbers = (int*)malloc(length * sizeof(int));
+    int* numbers = (int*)malloc(length * sizeof(int));
     for (int i = 0; i < length; i++)
     {
         printf("numbers[%d] = ", i);
@@ -41,9 +37,12 @@ int main()
     printf("n = ");
     scanf_s("%d", &n);
     arraySwap(numbers, 0, m);
-    arraySwap(numbers, m, n);
+    arraySwap(numbers, m + 1, m + n);
     arraySwap(numbers, 0, m + n);
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < m + n; ++i)
+    {
         printf("%d ", numbers[i]);
+    }
     free(numbers);
 }
+
