badaffc721a6b2c60f981e638665963824117b14
whflf <79448063+whflf@users.noreply.github.com>
2023-09-23 17:27:30 +0300

    Update task3.c

--- task3.c
+++ task3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 int main()
 {
@@ -10,33 +11,35 @@ int main()
     printf("number2 = ");
     scanf_s("%d", &number2);
     int dividend = number1;
+    int divisor = number2;
     int answer = 0;
-    bool minusFlag = false;
-    if (number1 < 0 ^ number2 < 0)
-    {
-        minusFlag = true;
-    }
     if (number2 == 0)
     {
         printf("Zero division is prohibited ");
         return -1;
     }
-    if (number1 < 0)
-    {
-        number1 = -number1;
-    }
-    if (number2 < 0)
-    {
-        number2 = -number2;
+    number1 = abs(number1);
+    number2 = abs(number2);
+    if (number1 % number2 == 0) {
+        printf("%d / %d = %d", dividend, divisor, dividend / divisor);
+        return;
     }
     while (number1 > number2)
     {
         number1 -= number2;
         answer += 1;
     }
-    if (minusFlag)
+    if (dividend < 0 && divisor > 0)
     {
         answer = -answer - 1;
     }
-    printf("%d / %d = %d", dividend, number2, answer);
+    if (dividend < 0 && divisor < 0)
+    {
+        ++answer;
+    }
+    else if (dividend > 0 && divisor < 0) 
+    {
+        answer = -answer;
+    }
+    printf("%d / %d = %d", dividend, divisor, answer);
 }
