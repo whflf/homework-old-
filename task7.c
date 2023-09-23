ec6ca51e436d79944ddac718ff7399d22c2e1010
whflf <79448063+whflf@users.noreply.github.com>
2023-09-23 21:08:03 +0300

    Update task7.c

--- task7.c
+++ task7.c
@@ -4,25 +4,29 @@
 
 #define LENGTH 100
 
-int substringsCount(char string[], char string1[])
+int substringsCount(const char string[], const char string1[])
 {
     int index = 0;
     int count = 0;
-    const stringLength = strlen(string);
-    for (int i = 0; i < stringLength; i++)
+    const int stringLength = strlen(string);
+    const int string1Length = strlen(string1);
+    for (int i = 0; i < stringLength; ++i)
     {
-        if (string[i] == string1[index])
+        if (string[i] == string1[0])
         {
-            index += 1;
-            if (index == strlen(string1) - 1)
+            int substringFlag = 1;
+            for (int j = 1; j < string1Length - 1; ++j) 
             {
-                index = 0;
-                count += 1;
+                if (string[i + j] != string1[j]) 
+                {
+                    substringFlag = 0;
+                    break;
+                }
             }
-        }
-        else if (index != 0)
-        {
-            index = 0;
+            if (substringFlag == 1) 
+            {
+                ++count;
+            }
         }
     }
     return count;
